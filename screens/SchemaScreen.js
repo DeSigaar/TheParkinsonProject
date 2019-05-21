89dd0530015f064b70e773d7e604e790fcae9f6d
Mike Hendriks <info@mikehendriks.nl>
2019-05-21 12:40:12 +0200

    Schema now dynamic

--- screens/SchemaScreen.js
+++ screens/SchemaScreen.js
@@ -15,87 +15,81 @@ class SchemaScreen extends Component {
     navigation: PropTypes.object,
     logOut: PropTypes.func,
     authError: PropTypes.string,
-    user: PropTypes.object
+    user: PropTypes.object,
+    moments: PropTypes.array
+  };
+
+  getMoments = () => {
+    const { moments } = this.props;
+    if (moments) {
+      return moments.map(moment => {
+        return (
+          <SchemaMomentIndicator moment={moment.name} key={moment.id}>
+            {this.loopItems(moment.medicines, "medicines")}
+            {this.loopItems(moment.exercises, "exercises")}
+            {this.loopItems(moment.activities, "activities")}
+          </SchemaMomentIndicator>
+        );
+      });
+    } else {
+      return <Text>Geen informatie voor vandaag</Text>;
+    }
+  };
+
+  getDay = () => {
+    var d = new Date();
+    var weekdays = new Array(7);
+    weekdays[0] = "sunday";
+    weekdays[1] = "monday";
+    weekdays[2] = "tuesday";
+    weekdays[3] = "wednesday";
+    weekdays[4] = "thursday";
+    weekdays[5] = "friday";
+    weekdays[6] = "saturday";
+    return weekdays[d.getDay()];
+  };
+
+  loopItems = (array, type) => {
+    return array.map(item => {
+      let title, img, gradient, description;
+
+      switch (type) {
+        default:
+        case "medicines":
+          title = "Neem uw medicijn";
+          description = item.name;
+          img = require("../assets/images/icon/home/medicatie.png");
+          gradient = Gradients.blue;
+          break;
+        case "exercises":
+          title = "Doe uw oefeningen";
+          description = item.name;
+          img = require("../assets/images/icon/home/oefeningen.png");
+          gradient = Gradients.green;
+          break;
+        case "activities":
+          title = item.name;
+          img = require("../assets/images/icon/home/activiteiten.png");
+          gradient = Gradients.red;
+          break;
+      }
+
+      if (type == "exercises" && !item.days[this.getDay()]) {
+        return;
+      } else {
+        return <SchemaItem title={title} description={description} img={img} gradientColor={gradient} key={item.id} />;
+      }
+    });
   };
 
   render() {
-    const { navigation, logOut, authError, user } = this.props;
+    const { navigation } = this.props;
 
     return (
       <>
         <Header navigation={navigation} title="Schema" />
         <ScrollView>
-          <View style={styles.container}>
-            <SchemaMomentIndicator moment="Ochtend">
-              <SchemaItem
-                title={"Neem uw medicijn"}
-                description={"Dopamine - 200mg"}
-                img={require("../assets/images/icon/home/medicatie.png")}
-                gradientColor={Gradients.blue}
-              />
-              <SchemaItem
-                title={"Stretches"}
-                description={"Arm, been en rug stretches"}
-                img={require("../assets/images/icon/home/oefeningen.png")}
-                gradientColor={Gradients.green}
-              />
-            </SchemaMomentIndicator>
-
-            <SchemaMomentIndicator moment="Middag">
-              <SchemaItem
-                title={"Stretches"}
-                description={"Arm, been en rug stretches"}
-                img={require("../assets/images/icon/home/oefeningen.png")}
-                gradientColor={Gradients.green}
-              />
-            </SchemaMomentIndicator>
-            <SchemaMomentIndicator moment="Ochtend">
-              <SchemaItem
-                title={"Neem uw medicijn"}
-                description={"Dopamine - 200mg"}
-                img={require("../assets/images/icon/home/medicatie.png")}
-                gradientColor={Gradients.blue}
-              />
-              <SchemaItem
-                title={"Stretches"}
-                description={"Arm, been en rug stretches"}
-                img={require("../assets/images/icon/home/oefeningen.png")}
-                gradientColor={Gradients.green}
-              />
-            </SchemaMomentIndicator>
-
-            <SchemaMomentIndicator moment="Middag">
-              <SchemaItem
-                title={"Stretches"}
-                description={"Arm, been en rug stretches"}
-                img={require("../assets/images/icon/home/oefeningen.png")}
-                gradientColor={Gradients.green}
-              />
-            </SchemaMomentIndicator>
-            <SchemaMomentIndicator moment="Ochtend">
-              <SchemaItem
-                title={"Neem uw medicijn"}
-                description={"Dopamine - 200mg"}
-                img={require("../assets/images/icon/home/medicatie.png")}
-                gradientColor={Gradients.blue}
-              />
-              <SchemaItem
-                title={"Stretches"}
-                description={"Arm, been en rug stretches"}
-                img={require("../assets/images/icon/home/oefeningen.png")}
-                gradientColor={Gradients.green}
-              />
-            </SchemaMomentIndicator>
-
-            <SchemaMomentIndicator moment="Middag">
-              <SchemaItem
-                title={"Stretches"}
-                description={"Arm, been en rug stretches"}
-                img={require("../assets/images/icon/home/oefeningen.png")}
-                gradientColor={Gradients.green}
-              />
-            </SchemaMomentIndicator>
-          </View>
+          <View style={styles.container}>{this.getMoments()}</View>
         </ScrollView>
       </>
     );
@@ -112,20 +106,14 @@ const styles = StyleSheet.create({
 const mapStateToProps = (state, ownProps) => {
   return {
     ...ownProps,
-    authError: state.auth.authError,
-    user: state.firebase.profile
+    user: state.firebase.profile,
+    moments: state.firebase.profile.moments
   };
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-    ...ownProps,
-    logOut: () => {
-      dispatch(logOut());
-    },
-    setExpoPushToken: (uid, token) => {
-      dispatch(setExpoPushToken(uid, token));
-    }
+    ...ownProps
   };
 };
 
