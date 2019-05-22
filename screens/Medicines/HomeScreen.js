4e62009090cd9316cd00e25315863320dc097c10
Mike Hendriks <info@mikehendriks.nl>
2019-05-22 09:46:30 +0200

    Medication overview fixed

--- screens/Medicines/HomeScreen.js
+++ screens/Medicines/HomeScreen.js
@@ -1,44 +1,66 @@
 import React, { Component } from "react";
-import { Alert } from "react-native";
+import { StyleSheet, Text, View, Platform, ScrollView } from "react-native";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { SchemaItem, SchemaMomentIndicator } from "../../components/schema";
 import Gradients from "../../constants/Gradients";
+import Colors from "../../constants/Colors";
 
-import { Header, Container } from "../../components/common";
+import { Header } from "../../components/common";
 
 class HomeScreen extends Component {
   static propTypes = {
     navigation: PropTypes.object,
     user: PropTypes.object,
-    moments: PropTypes.array,
+    moments: PropTypes.object,
     medicines: PropTypes.array
   };
 
-  // constructor(props) {
-  //   super(props);
-
-  //   let { moments } = this.props;
-  //   moments.forEach((moment, i) => {
-  //     moments[i] = {
-  //       ...moment,
-  //       count: 0
-  //     };
-  //   });
-
-  //   this.state = {0
-  //     moments
-  //   };
-  // }
-
-  render() {
-    const { navigation, moments } = this.props;
+  //new
+  getUniqueMedicines = () => {
+    const { moments } = this.props;
+    let uniqueMedicine = new Array();
 
     if (moments) {
-      moments.forEach((moment, i) => {
-        // console.log(moment.exercises);
+      moments.forEach(moment => {
+        moment.medicines.forEach(medicine => {
+          if (uniqueMedicine.filter(m => m.name === medicine.name).length === 0) {
+            uniqueMedicine.push(medicine);
+          }
+        });
       });
+
+      return uniqueMedicine;
     }
+  };
+
+  showMedicinesContainer = () => {
+    const medicines = this.getUniqueMedicines();
+    if (medicines) {
+      return <SchemaMomentIndicator moment="A">{this.loopMedicines(medicines)}</SchemaMomentIndicator>;
+    } else {
+      //image (medicine not found, see adobeXD)
+      return <Text>Geen medicijnen ingesteld</Text>;
+    }
+  };
+
+  loopMedicines = array => {
+    return array.map(item => {
+      return (
+        <SchemaItem
+          key={item.id}
+          title={item.name}
+          description={"description"}
+          img={require("../../assets/images/icon/home/medicatie.png")}
+          gradientColor={Gradients.blue}
+        />
+      );
+    });
+  };
+
+  render() {
+    const { navigation, user, moments } = this.props;
+    const { getParam } = navigation;
 
     return (
       <>
@@ -47,102 +69,26 @@ class HomeScreen extends Component {
           title="Medicatie"
           actionType1="add"
           actionType2="delete"
-          actionType3="error"
-          actionPress1={() => navigation.navigate("MedicinesAdd")}
-          actionPress2={() => Alert.alert("Action2 is empty!")}
-          actionPress3={() => Alert.alert("Action3 is empty!")}
-          amountActions={3}
+          actionType3="arrow-left"
+          // actionPress1={() => navigation.navigate("Exercise")}
         />
-        <Container type="ScrollView">
-          <SchemaMomentIndicator moment="N">
-            <SchemaItem
-              title={"Naproxen"}
-              description={"Naproxen 250mg"}
-              // img={require("../assets/images/icon/home/medicatie.png")}
-              gradientColor={Gradients.blue}
-            />
-            <SchemaItem
-              title={"Naproxen"}
-              description={"Naproxen 250mg"}
-              // img={require("../assets/images/icon/home/medicatie.png")}
-              gradientColor={Gradients.blue}
-            />
-            <SchemaItem
-              title={"Naproxen"}
-              description={"Naproxen 250mg"}
-              // img={require("../assets/images/icon/home/medicatie.png")}
-              gradientColor={Gradients.blue}
-            />
-          </SchemaMomentIndicator>
-          <SchemaMomentIndicator moment="N">
-            <SchemaItem
-              title={"Naproxen"}
-              description={"Naproxen 250mg"}
-              // img={require("../assets/images/icon/home/medicatie.png")}
-              gradientColor={Gradients.blue}
-            />
-          </SchemaMomentIndicator>
-          <SchemaMomentIndicator moment="N">
-            <SchemaItem
-              title={"Naproxen"}
-              description={"Naproxen 250mg"}
-              // img={require("../assets/images/icon/home/medicatie.png")}
-              gradientColor={Gradients.blue}
-            />
-            <SchemaItem
-              title={"Naproxen"}
-              description={"Naproxen 250mg"}
-              // img={require("../assets/images/icon/home/medicatie.png")}
-              gradientColor={Gradients.blue}
-            />
-            <SchemaItem
-              title={"Naproxen"}
-              description={"Naproxen 250mg"}
-              // img={require("../assets/images/icon/home/medicatie.png")}
-              gradientColor={Gradients.blue}
-            />
-          </SchemaMomentIndicator>
-          <SchemaMomentIndicator moment="N">
-            <SchemaItem
-              title={"Naproxen"}
-              description={"Naproxen 250mg"}
-              // img={require("../assets/images/icon/home/medicatie.png")}
-              gradientColor={Gradients.blue}
-            />
-          </SchemaMomentIndicator>
-          <SchemaMomentIndicator moment="N">
-            <SchemaItem
-              title={"Naproxen"}
-              description={"Naproxen 250mg"}
-              // img={require("../assets/images/icon/home/medicatie.png")}
-              gradientColor={Gradients.blue}
-            />
-            <SchemaItem
-              title={"Naproxen"}
-              description={"Naproxen 250mg"}
-              // img={require("../assets/images/icon/home/medicatie.png")}
-              gradientColor={Gradients.blue}
-            />
-            <SchemaItem
-              title={"Naproxen"}
-              description={"Naproxen 250mg"}
-              // img={require("../assets/images/icon/home/medicatie.png")}
-              gradientColor={Gradients.blue}
-            />
-          </SchemaMomentIndicator>
-          <SchemaMomentIndicator moment="N">
-            <SchemaItem
-              title={"Naproxen"}
-              description={"Naproxen 250mg"}
-              // img={require("../assets/images/icon/home/medicatie.png")}as
-              gradientColor={Gradients.blue}
-            />
-          </SchemaMomentIndicator>
-        </Container>
+        <ScrollView>
+          <View style={[styles.container, Platform.OS === "ios" && styles.ios]}>{this.showMedicinesContainer()}</View>
+        </ScrollView>
       </>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 75,
+    padding: 20
+  },
+  ios: {
+    marginTop: 100
+  }
+});
 
 const mapStateToProps = (state, ownProps) => {
   return {
